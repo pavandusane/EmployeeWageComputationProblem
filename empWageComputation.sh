194a40cadb7585134393aa7287b96274af57d149
pavandusane <dusanepv7@gmail.com>
2020-09-29 19:55:13 +0530

    Added : total monthly wages functionality added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,24 +1,59 @@
+#!/bin/bash -x
+
 echo "Welcome to Employee Wage Computation Problem"
 isPresent=1
-randomcheck=$(( 1+$RANDOM%2 ))
 wageperhour=20
 fulldayhour=8
 parttimehour=4
-totalday=20
+totaldays=0
+totalhours=0
+dailywage=0
+parttimewage=0
+monthlyfulltimewage=0
+monthlyparttimewage=0
+persontype=$(( 1+$RANDOM%2 ))
+echo $persontype
+noofdays=20
 
-case $randomcheck in
+
+while [ true ]
+do
+   if [ $totalhours -lt 100 ]
+then
+   if [ $totaldays -lt 20 ]
+then
+
+	randomcheck=$(( 1+$RANDOM%2 ))
+	case $randomcheck in
 
 	1)
-	     echo "Employee is Present"
-	     dailywage=$(($wageperhour*$fulldayhour*$randomcheck))
-	     parttimewage=$(($wageperhour*$parttimehour*$randomcheck))
+              echo "Employee is Present"
+	      if [ $persontype -eq 1 ]
+	         then
+	         dailywage=$(($wageperhour*$fulldayhour*$randomcheck))
+	         monthlyfulltimewage=$(( $monthlyfulltimewage + $dailywage ))
+	         let totalhours+=8
+	      else
+                 parttimewage=$(($wageperhour*$parttimehour*$randomcheck))
+		 monthlyparttimewage=$(( $monthlyparttimewage + $parttimewage ))
+	         let totalhours+=4
+	      fi
+	          let totaldays++
 	;;
+
 	2)
-
 		echo "Employee is not Present"
-esac
+		let totaldays++
 
-echo "Daily Employee Wage : $dailywage"
-echo "Part time wage :  $parttimewage"
-monthlywage=$(($dailywage*$totalday))
-echo "Monthly Wage : $monthlywage"
+	;;
+esac
+	else
+		exit
+    fi
+	else
+		exit
+    fi
+
+done
+echo "Monthly Fulltime wage : $monthlyfulltimewage "
+echo "Monthly Parttime wage : $monthlyparttimewage "
